6418465d294f017ef71b5d3fb6dc3f71483d58fd
Mahmoud Magdy <engmahmoud2016@gmail.com>
2016-08-02 11:34:52 +0200

    Update chatController.js

--- scripts/controllers/chatController.js
+++ scripts/controllers/chatController.js
@@ -6,47 +6,47 @@ chatController.$inject = ['$scope','$http', '$routeParams'];
 
 function chatController($scope, $http, $routeParams){
 
+    var sock = new SockJS('http://');
 
-     $scope.messages = [];
-     $scope.new_reply='';
+    $scope.messages = [];
 
     $http.get('scripts/data.json').success(function(data){
-    $scope.groupname = data;
+    $scope.details = data;
     $scope.whichGroup=$routeParams.groupId;
 
-  });
+    });
+  
+    $scope.sendMessage  = function () {
+         sock.send($scope.new_reply);
+         $scope.messages.push(this.new_reply);
+         $scope.new_reply='';
+     };
 
-	/**function getchatdetails($routeParams.groupId){
-		return $http({
+    sock.onmessage = function(e) {
+            $scope.messages.push(e.data);
+            $scope.$apply();
+        };
+    
+/**
+    function getchatdetails($routeParams.groupId){
+        return $http({
             method: 'GET',
             url: 'scripts/data.json' + groupId,
             crossDomain: true,
             xhrFields: { withCredentials: true },
         }).success(function(data) {
                 $scope.details = data;
+                $scope.messages.push(details.new_message);
             });
-    }**/
-
-   
-
-
- $scope.submit = function () {
-
-
-       $scope.messages.push(this.new_reply);
-       $scope.new_reply='';
-            }
     }
 
 
-/**function Addreply() {
+    function sendMessage() {
 
         var reply = {
-
             "user_id": $cookies.get("user_id"),
             "description": $scope.new_reply,
         };
-
         $http({
                 method: 'POST',
                 url: '',
@@ -54,7 +54,6 @@ function chatController($scope, $http, $routeParams){
                 crossDomain: true,
                 xhrFields: { withCredentials: true },
                 headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
-
             })
             .success(function(data, status, headers, config) {
                 $scope.new_reply = "";
@@ -62,4 +61,7 @@ function chatController($scope, $http, $routeParams){
 
             })
     }**/
+    
+    }
+
 
